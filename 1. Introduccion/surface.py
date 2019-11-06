c6155f61c82681fb17c5523ea5a46405a8ea7400
Florencio Ángel Luis Moya <faluis@atsistemas.com>
2019-11-06 17:18:21 +0100

    Subida de la pantalla de juego

---
+++ 1. Introduccion/surface.py
@@ -0,0 +1,15 @@
+import pygame
+import sys
+
+pygame.init()
+
+width = 400
+height = 500
+
+surface = pygame.display.set_mode( (width, height) )
+pygame.display.set_caption("pygame game")
+while  True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
